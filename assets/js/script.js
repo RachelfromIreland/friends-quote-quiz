703101fbb39450ebf36a05b8da65a7c60237bd8b
Rachel McGlinchey <rachelfromireland@gmail.com>
2024-01-18 20:01:11 +0000

    Add welcome message and quiz div, and function to hide welcome message and start quiz

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/**
+ * Function which hides welcome message and displays the quiz
+ */
+function showQuiz() {
+    document.getElementById("welcome").style.display = "none"
+    document.getElementById("quiz").style.display = "block"
+}
