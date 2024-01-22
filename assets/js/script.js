35c4a22efac125a740ffd3fa9bee2e102ca60bff
Rachel McGlinchey <rachelfromireland@gmail.com>
2024-01-22 19:24:39 +0000

    Uncomment quotes and remove function console log.  Repeat questions prevented

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,10 +24,10 @@ const quizInfo = [
     friend: "Chandler",
     quotes: [
       "I'm funny, right? What do you know? You're a door. You only like knock-knock jokes.",
-      //"I'm a gym member. I try to go four times a week, but I've missed the last... twelve-hundred times.",
-      //"I tend to keep talking until somebody stops me.",
-      //"You know, on second thought, gum would be perfection.",
-      // "I can handle this. Handle's my middle name. Actually, it's the middle part of my first name.",
+      "I'm a gym member. I try to go four times a week, but I've missed the last... twelve-hundred times.",
+      "I tend to keep talking until somebody stops me.",
+      "You know, on second thought, gum would be perfection.",
+      "I can handle this. Handle's my middle name. Actually, it's the middle part of my first name.",
     ],
   },
 
@@ -35,10 +35,10 @@ const quizInfo = [
     friend: "Monica",
     quotes: [
       "And remember, if I'm harsh with you, it's only because you're doing it wrong.",
-      //"I've got this uncontrollable need to please people.",
-      //"Fine. I'd like to meet this chicken expert. Send the colonel in.",
-      //"Welcome to the real world. It sucks. You're gonna love it.",
-      //"Lips moving, still talking!",
+      "I've got this uncontrollable need to please people.",
+      "Fine. I'd like to meet this chicken expert. Send the colonel in.",
+      "Welcome to the real world. It sucks. You're gonna love it.",
+      "Lips moving, still talking!",
     ],
   },
 
@@ -47,9 +47,9 @@ const quizInfo = [
     quotes: [
       " I'm curvy, and I like it",
       "Sup with the wack playstation, sup",
-      // "Look at me! I'm Chandler! could I be wearing any more clothes?",
-      // "(This friend) doesn't share food!",
-      //"Well, the fridge broke. So I had to eat everything.",
+      "Look at me! I'm Chandler! could I be wearing any more clothes?",
+      "(This friend) doesn't share food!",
+      "Well, the fridge broke. So I had to eat everything.",
     ],
   },
 
@@ -58,9 +58,9 @@ const quizInfo = [
     quotes: [
       "No uterus, no opinion.",
       "Oh, I'm sorry. Did my back hurt your knife?",
-      // "Oh, that's okay, girls tend to not like me.",
-      //  "Everyone is getting married or pregnant or promoted and I'm getting coffee! \nAnd it's not even for me!",
-      // "Isn't that just kick-you-in-the-crotch, spit-on-your-neck fantastic?",
+      "Oh, that's okay, girls tend to not like me.",
+      "Everyone is getting married or pregnant or promoted and I'm getting coffee! \nAnd it's not even for me!",
+      "Isn't that just kick-you-in-the-crotch, spit-on-your-neck fantastic?",
     ],
   },
 
@@ -69,9 +69,9 @@ const quizInfo = [
     quotes: [
       "Oh, I wish I could, but I don't want to.",
       "Something is wrong with the left phalange.",
-      //  "They don't know that we know they know we know.",
-      //  "See? He's her lobster!",
-      //  "Meet Princess Consuela Banana Hammock",
+      "They don't know that we know they know we know.",
+      "See? He's her lobster!",
+      "Meet Princess Consuela Banana Hammock",
     ],
   },
 
@@ -80,14 +80,16 @@ const quizInfo = [
     quotes: [
       "You-you-you-you threw my sandwich away? My sandwich? MY SANDWICH?!",
       "Ah. Humor based on my pain.",
-      //  "I tell you, when I actually die, some people are going to get seriously haunted!",
-      // "WE WERE ON A BREAK!",
+      "I tell you, when I actually die, some people are going to get seriously haunted!",
+      "WE WERE ON A BREAK!",
+      "UBERWEISS! It's new, it's German, it's EXTRA TOUGH!",
     ],
   },
 ];
 
 let questionCounter = 0;
 let questionsAsked = [];
+
 /**
  * Function to pick a character at random and then one of their quotes at random and display in the random quote div.
  * Also increments questionCounter variable by 1 and defines activeQuestionCharacter for use globally
@@ -116,10 +118,8 @@ function displayQuestion() {
     questionCharacter: questionCharacter.friend,
     randomQuote: randomQuote,
   });
+
   console.log(questionsAsked);
-  console.log(
-    hasQuoteRepeated(questionCharacter.friend, randomQuote, questionsAsked)
-  );
 
   let quoteDiv = document.getElementById("random-quote");
 
@@ -250,6 +250,8 @@ function beginAgain() {
 
   document.getElementById("welcome").style.display = "block";
   document.getElementById("quiz").style.display = "none";
+
+  questionsAsked = [];
 }
 
 /**
